9a9b99b49fed19dfadb8f47d21880229689495be
Kohki SHIKATA <indigoworks@gmail.com>
2024-09-09 18:37:49 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <p>
+      This is an auto-imported component
+    </p>
+    <div>
+    <button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+</div>
+  </div>
+</template>
